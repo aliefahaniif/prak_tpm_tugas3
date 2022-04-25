6474b62a2afd61bc767d8b6cb503f1c2ea1cef37
ALIEFA HANIIF <aliefa.haniif12@gmail.com>
2022-04-26 00:29:18 +0700

    menambahkan tugas 3

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:tugas3/page_detail_countries.dart';
+
+
+void main() async{
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: PageDetailCountries()
+    );
+  }
+}
